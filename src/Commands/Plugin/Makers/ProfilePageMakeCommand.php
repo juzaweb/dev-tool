47b6a57217eace774e18cbc1ab23461d7ed261fd
Juzaweb <dangtheanh16@gmail.com>
2024-04-27 10:52:01 +0700

    :+1: Move translation to plugin

---
+++ src/Commands/Plugin/Makers/ProfilePageMakeCommand.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * JUZAWEB CMS - Laravel CMS for Your Project
+ *
+ * @package    juzaweb/cms
+ * @author     The Anh Dang
+ * @link       https://juzaweb.com
+ * @license    GNU V2
+ */
+
+namespace Juzaweb\DevTool\Commands\Plugin\Makers;
+
+use Juzaweb\DevTool\Abstracts\GeneratorCommand;
+use Juzaweb\DevTool\Traits\ModuleCommandTrait;
+
+class ProfilePageMakeCommand extends GeneratorCommand
+{
+    use ModuleCommandTrait;
+
+    protected function getDestinationFilePath()
+    {
+        // TODO: Implement getDestinationFilePath() method.
+    }
+
+    protected function getTemplateContents()
+    {
+        // TODO: Implement getTemplateContents() method.
+    }
+}
